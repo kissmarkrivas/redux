e0fdb678aceb1c0bd1143020dc0502a832d066a2
kissmark <rivaskissmark@gmail.com>
2023-03-26 23:43:46 -0500

    add repo

--- 03-todoapp/src/app/todos/todo.reducer.ts
+++ 03-todoapp/src/app/todos/todo.reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
+import { crear, editar, toggle } from './todo.actions';
 import { Todo } from './models/todo.model';
-import { crear } from './todo.actions';
 
 export const estadoInicial:Todo[] = [
   new Todo('Salvemos al mundo'),
@@ -12,6 +12,33 @@ export const estadoInicial:Todo[] = [
 const _todoReducer = createReducer(
   estadoInicial,
   on(crear, (state, {texto}) => [...state,new Todo(texto)]),
+  on(toggle, (state, {id}) => {
+    return state.map(todo =>{
+      if(todo.id===id){
+        return{
+          ...todo,
+          completado:!todo.completado 
+  
+        }
+      }else{
+        return todo;
+      }
+    })
+  }),
+
+  on(editar, (state, {id,texto}) => {
+    return state.map(todo =>{
+      if(todo.id===id){
+        return{
+          ...todo,
+          texto:texto 
+  
+        }
+      }else{
+        return todo;
+      }
+    })
+  }),
 );
 
 export function todoReducer(state:any,action:any){
